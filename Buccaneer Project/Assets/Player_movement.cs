96befe3b44977f64b23e547ff99cf22b4f46352d
DominikCG <dominikc.gause@gmail.com>
2020-09-09 19:26:41 -0300

    0.0.1
    Base project

---
+++ Buccaneer Project/Assets/Player_movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+
+    public float movement_speed = 0f;
+    public Rigidbody2D player_rg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
