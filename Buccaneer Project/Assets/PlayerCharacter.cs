6eb5291cf8a18cdb637f7dbf369e4845a18996f5
DominikCG <dominikc.gause@gmail.com>
2022-05-10 23:48:59 -0300

    animation swing u/d/l/r  + move

--- Buccaneer Project/Assets/PlayerCharacter.cs
+++ Buccaneer Project/Assets/PlayerCharacter.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody2D playerRb = default;
+    [SerializeField] private float movementSpeed = default;
+    [SerializeField] private Vector2 axis = default;
+
+
 
     // Update is called once per frame
     void Update()
     {
+        axis.y = Input.GetAxis("Vertical");
+        axis.x = Input.GetAxis("Horizontal");
         
+    }   
+
+    void FixedUpdate(){
+
+        Move(movementSpeed,axis);      
+    }
+    private void Move(float ACC, Vector2 DIR)
+    {
+        playerRb.MovePosition(playerRb.position + DIR * ACC * Time.deltaTime);
     }
 }
